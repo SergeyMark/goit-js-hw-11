ae5d3c815391f55e959ac7121a9e520e3c0fd7c8
Sergey <110375004+SergeyMark@users.noreply.github.com>
2023-01-19 16:44:54 +0200

    add news file

---
+++ src/index.js
@@ -0,0 +1,378 @@
+import './css/styles.css'
+// import fetchCountries from "./fetchCountries";
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+
+
+
+const formSearch = document.querySelector('#search-form');
+const galleryList = document.querySelector('.gallery');
+const btnLoad = document.querySelector('.load-more');
+
+let page = 1;
+let perPage = 0;
+let searchQuery = '';
+
+
+// Запит на бекенд fetch
+function fetchSearchImage(page, searchQuery) {
+    const API_URL = 'https://pixabay.com/api/';
+    const KEY = '?key=32918808-7bed14219e5e11c2127636ebd';
+
+    return fetch(`${API_URL}${KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=400`).then(response => {
+        console.log(response);
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
+
+        return response.json();
+    });
+}
+
+
+// Пошук в search
+formSearch.addEventListener('submit', onSearch);
+
+function onSearch(event){ 
+    event.preventDefault();
+
+    searchQuery = event.currentTarget.searchQuery.value;
+    page = 1;
+
+    if(!searchQuery) {
+        galleryList.innerHTML = '';
+        return;
+    }
+
+    fetchSearchImage(page, searchQuery).then(imgSearchFeatch => {
+
+        console.log(imgSearchFeatch);
+
+        if(imgSearchFeatch === []){
+          // notiflix.
+          alert('"Sorry, there are no images matching your search query. Please try again."');
+        };
+
+        createCardImg(imgSearchFeatch.hits);
+
+        btnLoad.hidden = false;
+
+    }).catch(err => console.log(err));
+};
+
+
+// функція яка робить вертку картинки 
+function createCardImg(imgArr) {
+  galleryList.innerHTML = imgArr.map(img => 
+  `<div class="photo-card">
+      <img src="${img.webformatURL}" alt="${img.tags}" loading="lazy" class="photo-img" />
+      <div class="info">
+        <p class="info-item">
+          <b>Likes${img.likes}</b>
+        </p>
+        <p class="info-item">
+          <b>Views${img.views}</b>
+        </p>
+        <p class="info-item">
+          <b>Comments${img.comments}</b>
+        </p>
+        <p class="info-item">
+          <b>Downloads${img.downloads}</b>
+        </p>
+      </div>
+  </div>`
+  ).join('')
+};
+
+// при кліку загрузка ще контенту
+btnLoad.addEventListener('click', onButtonLoadMore);
+
+function onButtonLoadMore() {
+  page += 1;
+  
+  btnLoad.hidden = true;
+
+  fetchSearchImage(page, searchQuery).then(imgSearchFeatchMore => {
+    perPage += imgSearchFeatchMore.hits.length;
+
+    createCardImg(imgSearchFeatchMore.hits);
+
+    if (perPage >= imgSearchFeatchMore.totalHits) {
+      btnLoad.hidden = true;
+
+      // notiflix.
+      alert("We're sorry, but you've reached the end of search results.");
+    }
+
+    btnLoad.hidden = false;
+
+  }).catch(err => console.log(err));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // верстка пошуку країн
+// function createCountryMarkUp(arr) {
+//     countryList.innerHTML = arr.map(({flags: {svg}, name: {official}}) => 
+//         `<li>
+//             <img src="${svg}" alt="${official}" width="100">
+//             <h2>${official}</h2>
+//         </li>`
+// ).join('')};
+
+// function createCardImg(imgArr) {
+//     imgRander.innerHTML = imgArr.map((img) => 
+//     `<img src="${}" alt="${official}" width="50">
+//     <span>${official}</span>
+//     <div>
+//       <span>Capital:</span>
+//       <span>${capital}</span>
+//     </div>
+//     <div>
+//       <span>Population:</span>
+//       <span>${population}</span>
+//     </div>
+//     <div>
+//       <span>Languages:</span>
+//       <span>${Object.values(languages).join(', ')}</span>
+//     </div>`
+// ).join('')};
+
+
+// функция фетч возрощяет апи стран
+// function fetchCountries(name){
+//     const BASE_URL = 'https://restcountries.com/v3.1/name';
+//     const fields = '?fields=name,capital,population,flags,languages';
+
+//   return fetch(`${BASE_URL}${name}${fields}`)
+//     .then(response => {
+//         console.log(response);
+ 
+        
+//         if (!response.ok) {
+//             throw new Error(response.statusText);
+//         }
+
+//         return response.json();
+//     });
+// };
+
+// fetchCountries('peru');
+
+
+
+// функція евент по пошуку картинок та країн з завдання 10
+// formSearch.addEventListener('submit', onSearch);
+
+// function onSearch(event){ 
+//     event.preventDefault();
+
+//     let inputSearch = event.currentTarget.searchQuery.value;
+    
+//     if(!inputSearch) {
+//         galleryList.innerHTML = '';
+//         return;
+//     }
+
+//     fetchSearchImage(page, inputSearch).then(imgSearchFeatch => {
+//         console.log(imgSearchFeatch);
+
+//         createCardImg(imgSearchFeatch);
+
+//         // if(countryFetch.length > 10){
+//         //     // Notify.info();
+//         //     console.log('info')
+//         // }
+
+//         // if (countryFetch.length >= 2 && countryFetch.length <= 10) {
+//         //     // список країн
+//         //     createCountryMarkUp(countryFetch);
+//         //     countryInfo.innerHTML = '';
+//         // }
+
+//         // if (countryFetch.length === 1) {
+//         //     createCountryMarUpAll(countryFetch);
+//         //     countryList.innerHTML = '';
+//         // }
+//     }).catch(err => console.log(err));
+// };
+
+
+
+
+
+
+//  функція по кліку рендерить картку на екран
+// <button class="btn">click</button>
+
+// const btn = document.querySelector('.btn');
+
+// btn.addEventListener('click', onButtonClick);
+
+
+// const arr = [
+//   {
+//     url: './girl3.jpg',
+//     title: 'Girl',
+//     like: 12,
+//     views: 25,
+//     comment: 53,
+//   },
+//   {
+//     url: './girl3.jpg',
+//     title: 'Girl',
+//     like: 110,
+//     views: 252,
+//     comment: 520,
+//   },
+//   {
+//     url: './girl3.jpg',
+//     title: 'Girl',
+//     like: 198,
+//     views: 258,
+//     comment: 56,
+//   },
+// ]
+
+// //  верстка однієї картки
+// function createCardImg(arr) {
+//     galleryList.innerHTML = arr.map((img) => {
+//     `<div class="photo-card">
+//     <img src="${img.url}" alt="${img.title}" loading="lazy" />
+//     <div class="info">
+//       <p class="info-item">
+//         <b>Likes ${img.like}</b>
+//       </p>
+//       <p class="info-item">
+//         <b>Views ${img.views}</b>
+//       </p>
+//       <p class="info-item">
+//         <b>Comments ${img.comment}</b>
+//       </p>
+//       <p class="info-item">
+//         <b>Downloads</b>
+//       </p>
+//     </div>
+//   </div>`
+//     }).join('')};
+
+// function onButtonClick() {
+
+//   createCardImg(arr);
+// }
